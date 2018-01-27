965bc1f9ab2f993bfe41a41f96f44d7eec71b81a
Curtis Mason <cumason@bu.edu>
2018-01-27 01:52:22 -0500

    Classification Functional, also added probability values

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ import numpy as np
 import os, cv2, GenData
 
 
+
 def categorical(num_classes, classification):
     '''Creates Classification One Hot Values
 
@@ -23,83 +24,103 @@ def get_image_shape(filepath):
     :returns: a list of the image dimensions
         Example: [width, height, channels]'''
     width, height, channels = cv2.imread(filepath).shape
-    return [width * height * channels]
+    return [width, height, channels]
 
 
-filenames = os.listdir(GenData.FUNCTIONS_DIRECTORY_NAME)
-filepaths = [os.path.join(GenData.FUNCTIONS_DIRECTORY_NAME, filename) for filename in filenames]
-
-n_classes = len(filenames)
-input_shape = [None] + get_image_shape(filepaths[0])
-
-x = tf.placeholder("float", input_shape)
-y = tf.placeholder("float", [None, n_classes])
-
-logits = np.array([cv2.imread(filepath) for filepath in filepaths]).reshape([n_classes] + input_shape[1:])
-labels = np.array([categorical(n_classes, _) for _ in range(n_classes)])
-classification = {str(_): filepath for _, filepath in enumerate(filepaths)}
-
-
-
-keep_rate = 0.8
-keep_prob = tf.placeholder(tf.float32)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def maxpool2d(x):
-    #                        size of window         movement of window
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-
-def convolutional_neural_network(x, n_classes):
-    width, height, channels = cv2.imread("funcDirectory/sin.jpeg").shape
-    weights = {'W_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-               'W_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-               'W_fc': tf.Variable(tf.random_normal([(width // 3) * (height // 3) * 64, 1024])),
-               'out': tf.Variable(tf.random_normal([1024, n_classes]))}
-
-    biases = {'b_conv1': tf.Variable(tf.random_normal([32])),
-              'b_conv2': tf.Variable(tf.random_normal([64])),
-              'b_fc': tf.Variable(tf.random_normal([1024])),
-              'out': tf.Variable(tf.random_normal([n_classes]))}
-
-    x = tf.reshape(x, shape=[-1, width, height, 1])
-
-    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
-    conv1 = maxpool2d(conv1)
-
-    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
-    conv2 = maxpool2d(conv2)
-
-    fc = tf.reshape(conv2, [-1, 86016])
-    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']) + biases['b_fc'])
-    fc = tf.nn.dropout(fc, 0.8)
-
-    output = tf.matmul(fc, weights['out']) + biases['out']
-
-    return output
-
-def train_neural_network(x):
-    prediction = convolutional_neural_network(x, n_classes)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
-    optimizer = tf.train.AdamOptimizer().minimize(cost)
+def model(x, mode):
+    conv1 = tf.layers.conv2d(
+        inputs=x,
+        filters=32,
+        kernel_size=[5,5],
+        padding="same",
+        activation=tf.nn.relu
+    )
+    pool1 = tf.layers.max_pooling2d(
+        inputs=conv1,
+        pool_size=[2,2],
+        strides=2
+    )
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5,5],
+        padding="same",
+        activation=tf.nn.relu
+    )
+    pool2 = tf.layers.max_pooling2d(
+        inputs=conv2,
+        pool_size=[2,2],
+        strides=2
+    )
+    pool_flat = tf.layers.flatten(
+        inputs=pool2
+    )
+    dense = tf.layers.dense(
+        inputs=pool_flat,
+        units=1024,
+        activation=tf.nn.relu
+    )
+    dropout = tf.layers.dropout(
+        inputs=dense,
+        rate=0.4,
+        training=mode
+    )
+    out = tf.layers.dense(
+        inputs=dropout,
+        units=13,
+    )
+    return out
 
 
-
-
-    hm_epochs = 10
+def train_model(x, y, mode, logits, labels, iterations):
+    prediction = model(x, mode)
+    probabilities = tf.nn.softmax(prediction)
 
     with tf.Session() as sess:
+        loss = tf.losses.softmax_cross_entropy(
+            onehot_labels=y,
+            logits=prediction
+        )
+
+        optimizer = tf.train.AdamOptimizer().minimize(loss)
         sess.run(tf.global_variables_initializer())
 
-        for epoch in range(hm_epochs):
-            epoch_loss = 0
-            print('Optimization Begin')
-            _, c = sess.run([optimizer, cost], feed_dict={x: logits, y: labels})
-            print('Epoch', epoch, 'Finished, loss: ', c)
+        for i in range(iterations):
+            loss_val, opt_func = sess.run([loss, optimizer],
+                                          feed_dict={x:logits,
+                                                     y:labels,
+                                                     mode:True})
+            print("Loss {1}: {0}".format(loss_val, i))
+
+        return  (str(np.argmax(sess.run([prediction], feed_dict={
+            x:[logits[4]],
+            mode:False})[0])), sess.run([probabilities], feed_dict={x:[logits[4]], mode:False}))
 
 
-train_neural_network(x)
+
+def main():
+    filenames = os.listdir(GenData.FUNCTIONS_DIRECTORY_NAME)
+    filepaths = [os.path.join(GenData.FUNCTIONS_DIRECTORY_NAME, filename) for filename in filenames]
+
+    n_classes = len(filenames)
+    input_shape = [None] + get_image_shape(filepaths[0])
+
+    x = tf.placeholder("float", input_shape)
+    y = tf.placeholder("float", [None, n_classes])
+    mode = tf.placeholder(tf.bool, shape=())
+
+    logits = np.array([cv2.imread(filepath) for filepath in filepaths]).reshape([13]+input_shape[1:])
+    labels = np.array([categorical(n_classes, p) for p in range(n_classes)])
+    classification = {str(p): filepath for p, filepath in enumerate(filepaths)}
+    print(input_shape)
+    print(logits.shape)
+
+    keep_rate = 0.8
+    keep_prob = tf.placeholder(tf.float32)
+    result = train_model(x, y, mode, logits, labels, 40)
+    print(classification[result[0]])
+    print(result[1])
+
+
+if __name__ == "__main__":
+    main()
